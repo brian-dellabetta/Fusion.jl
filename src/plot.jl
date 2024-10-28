96653693b7b3fd8cb0e56b985f63d1d12cb8bf73
Brian Dellabetta <brian.dellabetta@gmail.com>
2024-10-28 18:08:34 -0500

    not right

--- src/plot.jl
+++ src/plot.jl
@@ -2,77 +2,130 @@ using GLMakie
 
 
 Line3f = Tuple{Point3f,Point3f}
+
 Base.@kwdef mutable struct Atom
     r::Point3f = Point3f(0.0, 0.0, 0.0)
+    edge_idx::Integer = 1
     is_spin_up::Bool = true
 end
 
 struct Lattice
-    points::Vector{Point3f}
+    points::Vector{Vector{Point3f}}
+    edge_points::Vector{Point3f}
     lines::Vector{Line3f}
 
     function Lattice(
         #domain
         domain::Line3f,
         #lattice constant
-        a0::Float32=2.0f0,
+        a0::Float32=6.0f0,
     )
         X = (domain[1][1], domain[2][1])
         Y = (domain[1][2], domain[2][2])
         Z = (domain[1][3], domain[2][3])
 
         #unit cell vectors
+        # https://pmc.ncbi.nlm.nih.gov/articles/PMC6116708/
         a = a0 * sqrt(3)
-        a1 = Point3f(0, a0, 0)
-        a2 = Point3f(a0 * sqrt(3) / 2, -a0 / 2, 0)
+        d1 = Point3f(0, a0, 0)
+        d2 = Point3f(a0 * sqrt(3) / 2, -a0 / 2, 0)
+        d3 = Point3f(-a0 * sqrt(3) / 2, -a0 / 2, 0)
+
+        a1 = Point3f(a0 / 2, a0 * sqrt(3) / 2, 0)
+        a2 = Point3f(a0 / 2, -a0 * sqrt(3) / 2, 0)
 
         #size
-        n_rows = ceil(Int, (Y[2] - Y[1]) / (a * sqrt(3)))
-        n_cols = ceil(Int, (X[2] - X[1]) / a)
+        ny = ceil(Int, (Y[2] - Y[1]) / (a0 * 3)) #(a * sqrt(3))
+        nx = ceil(Int, (X[2] - X[1]) / a)
 
-        points::Vector{Point3f} = []
         lines::Vector{Line3f} = []
+        points::Vector{Vector{Point3f}} = []
+        top_points::Vector{Point3f} = []
+        right_points::Vector{Point3f} = []
+        left_points::Vector{Point3f} = []
+        bottom_points::Vector{Point3f} = []
 
-        for col_idx in 0:n_cols-1, row_idx in 0:n_rows-1
-            #odd rows
-            p1 = Point3f(
-                X[1] + (col_idx * a0 * sqrt(3)),
-                Y[1] + (row_idx * a0 * (1 + sqrt(3))),
-                (Z[2] - Z[1]) / 2,
-            )
-            push!(points, p1)
-            if row_idx != n_rows - 1
-                push!(lines, (p1, p1 + a1))
+        for x_idx in 0:nx-1
+            odd_points::Vector{Point3f} = []
+            even_points::Vector{Point3f} = []
+            for y_idx in 0:ny-1
+                #odd rows
+                p1 = Point3f(
+                    X[1] + (x_idx * a0 * sqrt(3)),
+                    Y[1] + (y_idx * a0 * (1 + sqrt(3))),
+                    (Z[2] - Z[1]) / 2,
+                )
+                push!(odd_points, p1)
+                if y_idx != ny - 1
+                    push!(lines, (p1, p1 + d1))
+                end
+                if y_idx != ny - 1 || x_idx != 0
+                    push!(lines, (p1, p1 + d2))
+                end
+                if x_idx != 0
+                    push!(lines, (p1, p1 + d3))
+                end
+
+                #even rows
+                p2 = p1 - (d1 .* (sqrt(3) / 2)) + d2
+                push!(even_points, p2)
+                push!(lines, (p2, p2 + d1))
+                if (x_idx != nx - 1)
+                    push!(lines, (p2, p2 + d2))
+                end
+                if (x_idx != 0 || y_idx != 0)
+                    push!(lines, (p2, p2 + d3))
+                end
+
+                #edge points
+                if x_idx == 0
+                    push!(top_points, p1)
+                    push!(top_points, p1 + d1)
+                    push!(top_points, p1 + d2 - d1)
+                elseif y_idx == ny - 1
+                    push!(right_points, p1)
+                    push!(right_points, p2)
+                elseif x_idx == nx - 1
+                    push!(bottom_points, p1)
+                    push!(bottom_points, p2)
+                elseif y_idx == 0
+                    push!(left_points, p1)
+                    push!(left_points, p2)
+                end
             end
-            if row_idx != n_rows - 1 || col_idx != 0
-                push!(lines, (p1, p1 + a2))
-            end
-            if col_idx != 0
-                push!(lines, (p1, p1 + a2 .* (-1, 1, 1)))
-            end
-            #even rows
-            p2 = p1 - a1 + a2
-            push!(points, p2)
-            push!(lines, (p2, p2 + a1))
-            if (col_idx != n_cols - 1)
-                push!(lines, (p2, p2 + a2))
-            end
-            if (col_idx != 0 || row_idx != 0)
-                push!(lines, (p2, p2 + a2 .* (-1, 1, 1)))
-            end
+            push!(points, odd_points)
+            push!(points, even_points)
         end
 
-        new(points, lines)
+        new(
+            points,
+            # vcat(top_points, right_points, bottom_points[end:-1:1], left_points[end:-1:1]),
+            top_points[1:3],
+            lines
+        )
     end
 end
 
 function step!(a::Atom, l::Lattice)
-    dr = Point3f(10 * cos(a.r[1] / 10), 10 * cos(a.r[2] / 10), 0.0)
-    a.r += dr
+    if a.is_spin_up
+        if a.edge_idx == length(l.edge_points)
+            a.edge_idx = 1
+        else
+            a.edge_idx += 1
+        end
+    else
+        if a.edge_idx == 1
+            a.edge_idx = length(l.edge_points)
+        else
+            a.edge_idx -= 1
+        end
+    end
+
+    a.r = l.edge_points[a.edge_idx]
 end
 
 
-domain = (Point3f(-30.0, -30.0, -30.0), Point3f(30.0, 30.0, 30.0))
+domain = (Point3f(-30.0, -30.0, -10.0), Point3f(30.0, 30.0, 10.0))
 lattice_domain = (Point3f(-20.0, -20.0, 0.0), Point3f(20.0, 20.0, 0.0))
 attractor = Atom()
 lattice = Lattice(lattice_domain)
@@ -81,12 +134,12 @@ colors = Observable(Int[])
 
 set_theme!(theme_black())
 
-fig, ax, l = lines(points, color=colors,
+fig, ax, l = scatter(points, color=colors,
     colormap=:inferno, transparency=true,
     axis=(; type=Axis3, protrusions=(0, 0, 0, 0),
         viewmode=:fit, limits=(domain[1][1], domain[2][1], domain[1][2], domain[2][2], domain[1][3], domain[2][3])))
-ax.xticklabelsvisible = ax.yticklabelsvisible = ax.zticklabelsvisible = false
-ax.xlabel = ax.ylabel = ax.zlabel = ""
+# ax.xticklabelsvisible = ax.yticklabelsvisible = ax.zticklabelsvisible = false
+# ax.xlabel = ax.ylabel = ax.zlabel = ""
 
 linesegments!(lattice.lines, color=:grey95, linewidth=0.5)
 
@@ -94,10 +147,11 @@ record(fig, "lorenz.mp4", 1:120) do frame_idx
     for i in 1:50
         step!(attractor, lattice)
         push!(points[], attractor.r)
-        push!(colors[], 1)
+        push!(colors[], i)
     end
-    ax.azimuth[] = 1.7pi + 0.5 * sin(2pi * frame_idx / 120)
+    ax.azimuth[] = 1.7pi + 0.8 * sin(2pi * frame_idx / 120)
+    ax.elevation[] = pi / 5
     notify(points)
     notify(colors)
-    # l.colorrange = (0, frame_idx)
+    # l.colorrange = (0, frame_idx / 1000)
 end
