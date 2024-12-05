1b66c67f94aa8fbd65e2c20b101f010074b5c31d
Brian Dellabetta <brian.dellabetta@gmail.com>
2024-12-05 13:23:56 -0600

    hard barrier working starting point

--- src/Fusion.jl
+++ src/Fusion.jl
@@ -7,7 +7,8 @@ Line3f = Tuple{Point3f,Point3f}
 include("lattice.jl")
 include("atom.jl")
 include("plot.jl")
+include("particle_into_barrier.jl")
 
-export Atom, Lattice, create_movie, step!, Point3f, Line3f
+export Atom, Lattice, create_movie, step!, Point3f, Line3f, create_plot
 
 end
